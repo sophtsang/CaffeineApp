34dfc711e5eaf85d3cbb585b0c0d28b73ba61f20
sophtsang <sophiatsang19@gmail.com>
2025-12-05 15:30:48 -0500

    Implemented weekly card

--- app/src/main/java/com/example/caffiene/ui/screens/TrendsScreen.kt
+++ app/src/main/java/com/example/caffiene/ui/screens/TrendsScreen.kt
@@ -10,10 +10,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -29,8 +33,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,9 +45,12 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.caffiene.ui.viewmodels.CaffeineLog
 import com.example.caffiene.ui.viewmodels.TrackerViewModel
+import java.lang.System
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import kotlin.Long
+import kotlin.String
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -101,18 +111,18 @@ fun CollapsableCard(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+        ) {
             Text(
                 text = title,
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color(0xFF3A332C)
             )
-            Spacer(Modifier.height(10.dp))
-            if (expanded) {
-                Spacer(modifier = Modifier.height(8.dp))
-                content()
-            }
+        }
+        if (expanded) {
+            content()
         }
     }
 }
@@ -173,31 +183,93 @@ fun WeeklyIntakeCard(
         date to (dailyTotals[date] ?: 0f)
     }
     val maxIntake = (weeklyLogs.maxOfOrNull { it.second } ?: 1f)
+    val barGraphHeight by remember { mutableStateOf(200.dp) }
+    val barGraphWidth by remember { mutableStateOf(22.dp) }
+    val scaleYAxisWidth by remember { mutableStateOf(40.dp) }
+    val scaleLineWidth by remember { mutableStateOf(2.dp) }
 
     Column(
         modifier = Modifier
             .padding(16.dp)
-            .fillMaxWidth(),  // take full width of the Card
-        horizontalAlignment = Alignment.CenterHorizontally // center contents
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface),
+        verticalArrangement = Arrangement.Top,
     ) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.size(150.dp)
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(barGraphHeight),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Start
         ) {
-            weeklyLogs.forEach { (date, total) ->
-                val heightFraction = if (maxIntake == 0f) 0f else total / maxIntake
-                val barWeight = if (heightFraction > 0f) heightFraction else 0.01f
+            Box(
+                contentAlignment = Alignment.BottomCenter,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(scaleYAxisWidth)
+            ) {
                 Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.Bottom
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(barWeight)
-                            .background(if (heightFraction > 0f) Color(0xFF6F4E37) else Color.Transparent)
+                    Text(
+                        text = maxIntake.toString(),
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
                     )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(date.dayOfWeek.name.take(3)) // Mon, Tue, ...
+                    Spacer(modifier = Modifier.fillMaxHeight())
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Text(
+                        text = (maxIntake / 2).toString(),
+                        fontSize = 14.sp
+                    )
+                    Spacer(modifier = Modifier.fillMaxHeight(0.5f))
+                }
+            }
+
+            weeklyLogs.reversed().forEach { (_, total) ->
+                val heightFraction = if (maxIntake == 0f) 0f else total / maxIntake
+
+                Box(
+                    modifier = Modifier
+                        .padding(start = barGraphWidth, bottom = 5.dp)
+//                        .clip(CircleShape)
+                        .width(barGraphWidth)
+                        .fillMaxHeight(heightFraction)
+                        .background(
+                            color = if (heightFraction > 0f) Color(0xFF6E665D) else Color.Transparent,
+                            shape = RoundedCornerShape(6.dp)
+                        )
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(start = scaleYAxisWidth + barGraphWidth + scaleLineWidth)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(barGraphWidth)
+        ) {
+            weeklyLogs.reversed().forEach { (date, _) ->
+                Column() {
+                    Text(
+                        modifier = Modifier.width(barGraphWidth),
+                        text = date.dayOfWeek.name.take(2),
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        modifier = Modifier.width(barGraphWidth),
+                        text = date.dayOfMonth.toString(),
+                        fontSize = 14.sp,
+                        textAlign = TextAlign.Center
+                    )
                 }
             }
         }
@@ -210,4 +282,32 @@ fun WeeklyIntakeCard(
 @Composable
 fun PreviewTrendsScreen() {
     TrendsScreen()
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview
+@Composable
+fun PreviewWeeklyIntakeCard() {
+    val logs = listOf(
+        CaffeineLog (
+            beverageName = "Iced Americano",
+            caffeineAmt = "200.0"
+        ),
+        CaffeineLog (
+            beverageName = "Matcha",
+            caffeineAmt = "50.0",
+            timeStamp = LocalDateTime.of(2025, 12, 3, 13, 0)
+        ),
+        CaffeineLog (
+            beverageName = "Cold Brew",
+            caffeineAmt = "250.0",
+            timeStamp = LocalDateTime.of(2025, 12, 3, 8, 30)
+        )
+    )
+    val weeklyLogs = logs
+        .groupBy { it.timeStamp.toLocalDate() }
+        .mapValues { entry ->
+            entry.value.fold(0f) { acc, log -> acc + (log.caffeineAmt.toFloatOrNull() ?: 0f) }
+        }
+    WeeklyIntakeCard(weeklyLogs)
 }
